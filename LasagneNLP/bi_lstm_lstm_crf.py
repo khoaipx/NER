ebf738bcd5330dcf850c5e60f7e9e897d6a773fe
Hoang Pham <hoangpham@Hoangs-MacBook-Pro.local>
2017-05-19 17:28:49 +0700

    fix bi_lstm_lstm_crf

--- LasagneNLP/bi_lstm_lstm_crf.py
+++ LasagneNLP/bi_lstm_lstm_crf.py
@@ -61,7 +61,7 @@ def main():
                                                              output_size=char_embedd_dim, W=char_embedd_table,
                                                              name='char_embedding')
         layer_char_input = lasagne.layers.DimshuffleLayer(layer_char_embedding, pattern=(0, 2, 1))
-        return layer_char_embedding
+        return layer_char_input
 
     logger = utils.get_logger("BiLSTM-LSTM-CRF")
     fine_tune = args.fine_tune
