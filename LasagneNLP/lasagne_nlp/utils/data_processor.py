dee5f77a6701ce13b5ce8053a4467cf2192f3e36
pth1993 <phamthaihoang.hn@gmail.com>
2017-05-25 10:31:55 +0700

    25-5

--- LasagneNLP/lasagne_nlp/utils/data_processor.py
+++ LasagneNLP/lasagne_nlp/utils/data_processor.py
@@ -453,9 +453,9 @@ def load_dataset_sequence_labeling(train_path, dev_path, test_path, word_column=
     logger.info("Reading data from training set...")
     word_sentences_train, _, word_index_sentences_train, label_index_sentences_train = read_conll_sequence_labeling(
         train_path, word_alphabet, label_alphabet, word_column, label_column)
-    print word_sentences_train
-    print word_index_sentences_train
-    print label_index_sentences_train
+    print word_sentences_train[0]
+    #print word_index_sentences_train
+    #print label_index_sentences_train
 
     # if oov is "random" and do not fine tune, close word_alphabet
     if oov == "random" and not fine_tune:
