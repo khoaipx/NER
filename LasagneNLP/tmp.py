9d5f24c16d604cdf4187af9e1994037d6b6ff6f4
pth1993 <phamthaihoang.hn@gmail.com>
2017-07-05 10:49:21 +0700

    5-7

---
+++ LasagneNLP/tmp.py
@@ -0,0 +1,21 @@
+import codecs
+
+
+def load_word2vec(filename):
+    f = codecs.open(filename, 'r', 'utf-8', 'ignore')
+    word2vec_list = []
+    f.readline()
+    cnt = 0
+    for line in f:
+        cnt += 1
+        try:
+            word2vec_list.append(line.split()[0])
+        except:
+            print cnt
+            print line
+    f.close()
+    return word2vec_list
+
+
+if __name__ == '__main__':
+    load_word2vec('data/Vie_Skip_Gram_300.txt')
