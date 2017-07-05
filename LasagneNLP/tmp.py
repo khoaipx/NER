584e72b60063800ecf711432167c0343ff0f4db2
pth1993 <phamthaihoang.hn@gmail.com>
2017-07-05 11:02:09 +0700

    5-7

--- LasagneNLP/tmp.py
+++ LasagneNLP/tmp.py
@@ -19,5 +19,20 @@ def load_word2vec(filename):
     return word2vec_list
 
 
+def create_word2vec_new(filename1, filename2):
+    f1 = codecs.open(filename1, 'r', 'utf-8', 'ignore')
+    f2 = codecs.open(filename2, 'w', 'utf-8')
+    cnt = 0
+    for line in f1:
+        cnt += 1
+        if len(line) > 10:
+            f2.write(line)
+        else:
+            print cnt
+    f1.close()
+    f2.close()
+
+
 if __name__ == '__main__':
-    load_word2vec('data/Vie_Skip_Gram_300.txt')
+    #load_word2vec('data/Vie_Skip_Gram_300.txt')
+    create_word2vec_new('data/Vie_Skip_Gram_300.txt', 'data/Vie_Skip_Gram_300_new.txt')
