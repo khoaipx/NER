03a185da662b1ff2c3c0cc290b65d460ddb274ac
pth1993 <phamthaihoang.hn@gmail.com>
2017-07-06 14:22:23 +0700

    6-7

--- LasagneNLP/bi_lstm_lstm_crf.py
+++ LasagneNLP/bi_lstm_lstm_crf.py
@@ -115,7 +115,7 @@ def main():
     target_var = T.imatrix(name='targets')
     mask_var = T.matrix(name='masks', dtype=theano.config.floatX)
     mask_c_var = T.tensor3(name='masks-c', dtype=theano.config.floatX)
-    mask_slice_var = T.tensor4(name='masks-slice', dtype=theano.config.floatX)
+    mask_slice_var = T.itensor4(name='masks-slice')
     if fine_tune:
         input_var = T.imatrix(name='inputs')
         num_data, max_length = X_train.shape
